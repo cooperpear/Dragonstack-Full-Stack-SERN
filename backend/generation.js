f933cedaeb6844029e4a94812513df8fa89082b9
Cooper Pearson <mptprogesterone@gmail.com>
2022-01-03 13:37:27 -0600

    generation class

--- backend/generation.js
+++ backend/generation.js
@@ -1,5 +1,21 @@
+const { REFRESH_RATE, SECONDS } = require("./config");
+
+const refreshRate = REFRESH_RATE * SECONDS;
+
 class Generation {
     constructor() {
-        this.expiration = null;
+
+        this.expiration = this.calculateExpiration();
+
+    }
+
+    calculateExpiration() {
+        const expirationPeriod = Math.floor(Math.random() * (refreshRate / 2));
+
+        const msUntilExpiration = Math.random() < 0.5 ?
+            refreshRate - expirationPeriod :
+            refreshRate + expirationPeriod;
+
+        return new Date(Date.now() + msUntilExpiration);
     }
 }
